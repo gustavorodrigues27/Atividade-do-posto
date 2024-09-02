1e21cd31ff56c043ffe534caa2f36ec7946ebf93
gustavorodrigues27 <goncalves.gustavo27@escola.pr.gov.br>
2024-09-02 12:25:33 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Digite 1 para álcool, 2  para gasolina, 3 para diesel: ");
+    String numero = sc.next();
+    
+
+    if (numero.equals("1")){
+      System.out.println("Alcool");
+    }
+    
+
+
+
+
+
+
+
+    sc.close();
+  }
+
+  
+}
